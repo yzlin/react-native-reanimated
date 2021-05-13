dede5ee77a28d10740fe1334baff89664d698597
Jakub Gonet <jakub.gonet@swmansion.com>
2021-05-13 20:48:04 +0200

    Initial layout and cards

---
+++ docs/src/pages/showcase/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import Layout from '@theme/Layout';
+import styles from './styles.module.css';
+
+function Filter({ expanded }) {
+  const filterButton = (
+    <>
+      <button className="button button--secondary button--lg"> Filter ↑</button>
+    </>
+  );
+
+  const filterExpanded = (
+    <>
+      <div></div>
+    </>
+  );
+  return (
+    <>
+      {filterButton}
+      {expanded ?? filterExpanded}
+    </>
+  );
+}
+
+function Card() {
+  return (
+    <>
+      <article className={styles.card}>
+        <div
+          style={{
+            backgroundColor: 'black',
+            width: '380px',
+            height: '470px',
+          }}></div>
+        <div className={styles.cardText}>
+          <h2>Title</h2>
+          <p>by @JakubGonet on Twitter</p>
+        </div>
+      </article>
+    </>
+  );
+}
+
+function Showcase() {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <Filter />
+        <div className={styles.cardsGrid}>
+          {Array.from(Array(12)).map((_, i) => (
+            <Card key={i} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+export default Showcase;
